80ed8dbd58394b182c9d8f38464db9918200311e
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-05-18 09:49:30 -0400

    fix User model namespacing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Domain\User\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
