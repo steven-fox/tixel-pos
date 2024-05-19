13e9eb789dd6d5e0287be3fc955690a0eb44efcc
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-05-19 17:13:28 -0400

    alter webhook test to assert CallWebhookJob and payload

--- tests/Feature/PizzaStatusUpdateTest.php
+++ tests/Feature/PizzaStatusUpdateTest.php
@@ -6,11 +6,11 @@ use App\Domain\Pizza\Models\Pizza;
 use App\Domain\Pizza\ModelStates\Events\PizzaStateChanged;
 use App\Domain\Pizza\ModelStates\Pending;
 use App\Domain\Pizza\ModelStates\Started;
-use App\Domain\Support\Broadcasting\Channels\CustomerWebsiteWebhookChannel;
 use Illuminate\Broadcasting\BroadcastEvent;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Bus;
+use Spatie\WebhookServer\CallWebhookJob;
 use Tests\TestCase;
 
 class PizzaStatusUpdateTest extends TestCase
@@ -41,7 +41,7 @@ class PizzaStatusUpdateTest extends TestCase
 
     public function test_the_pizza_state_changed_event_can_be_broadcast_by_a_webhook_connection(): void
     {
-        Bus::fake();
+        Bus::fake([CallWebhookJob::class]);
         config()->set('customer-website.broadcast_using', 'webhook');
 
         $pizza = Pizza::factory()
@@ -51,13 +51,11 @@ class PizzaStatusUpdateTest extends TestCase
         // Transition pizza status: Pending -> Started
         $pizza->status->transitionTo(Started::class);
 
-        Bus::assertDispatched(BroadcastEvent::class, function (BroadcastEvent $broadcastEvent) use ($pizza) {
-            return $broadcastEvent->event instanceof PizzaStateChanged
-                   && $broadcastEvent->event->model->is($pizza)
-                   && $broadcastEvent->event->initialState instanceof Pending
-                   && $broadcastEvent->event->finalState instanceof Started
-                   && $broadcastEvent->event->broadcastOn() instanceof CustomerWebsiteWebhookChannel
-                   && $broadcastEvent->event->broadcastConnections() === ['webhook'];
+        Bus::assertDispatched(CallWebhookJob::class, function (CallWebhookJob $job) use ($pizza) {
+            return $job->webhookUrl === config('customer-website.webhook.url')
+               && $job->payload['event_type'] === 'pizza.status.updated'
+               && $job->payload['model']['id'] === $pizza->id
+               && $job->payload['model']['status']['name'] === Started::getMorphClass();
         });
     }
 }
